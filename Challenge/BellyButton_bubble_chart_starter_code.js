1a2970499cb1e023e291d2e867e5f1a762344cb0
Adam <akmilton11@gmail.com>
2021-07-10 16:54:13 -0400

    updated

---
+++ Challenge/BellyButton_bubble_chart_starter_code.js
@@ -0,0 +1,34 @@
+// Bar and Bubble charts 
+// Create the buildCharts function.
+function buildCharts(sample) {
+  // Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    
+
+    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot(); 
+
+    // 1. Create the trace for the bubble chart.
+    
+    trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      mode: 'markers'
+    };
+    var bubbleData = [trace1];
+
+  
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: { title: "OTU ID" },
+      yaxis: { title: "Samples" },
+      hovermode: 'closest',
+
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+  });
+}
